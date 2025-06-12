039db991b577b0dce3b9f0264ac6676a377e70a2
Abhishek <abhisheks@pearlorganisation.com>
2025-06-12 14:59:32 +0530

    test1

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,8 +1,7 @@
 // routes/orderRoutes.js
 const express = require('express');
 const router = express.Router();
-const orderController = require('../Controllers/orderController');
-
+const orderController = require('../controllar/orderController');
 router.post('/place-order', orderController.placeOrder);
 router.get('/get-orders', orderController.getOrders);
 router.get('/get-orders/:userId', orderController.getOrdersByUserId);
