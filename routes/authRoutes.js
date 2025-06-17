c88befbae46ef4f88beb37d4851e462cff107436
Abhishek <abhisheks@pearlorganisation.com>
2025-06-17 16:41:16 +0530

    test1

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -7,18 +7,17 @@ const {
   updateUser,
   getUserProfiles,
   getUserProfileById,
-  sendOtp,
-  verifyOtp
-} = require('../controllar/authController'); 
 
+} = require('../controllar/authController'); 
+const otpController = require('../Controllers/otpController');
 const { uploadProduct } = require('../middleware/upload');
 
 // Routes
 router.post('/signup', uploadProduct.single('profileImage'), signup);
 router.put('/update-user/:userId', uploadProduct.single('profileImage'), updateUser);
 router.post('/login', login);
-router.post('/send-otp', sendOtp);
-router.post('/verify-otp', verifyOtp);
+router.post('/send-otp', otpController.sendOtp);
+router.post('/verify-otp', otpController.verifyOtp);
 router.get('/get-user-profiles', getUserProfiles);
 router.get('/users/:userId', getUserProfileById);
 
