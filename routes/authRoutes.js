de7868268bc4ef0418685dd67d4e04b64f0d76bc
Abhishek <abhisheks@pearlorganisation.com>
2025-06-17 15:25:47 +0530

    test4

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { signup, login,updateUser,getUserProfiles,getUserProfileById } = require('../Controllers/authController');
+const { signup, login,updateUser,getUserProfiles,getUserProfileById } = require('../controllar/authController');
 const { uploadPrifle } = require('../middleware/upload');
 const otpController = require('../controllar/authController');
 
