33322bac92783a51c443f69e86ba6dfa65a694a4
Abhishek <abhisheks@pearlorganisation.com>
2025-06-20 15:02:40 +0530

    cors issue resolved

--- server.js
+++ server.js
@@ -13,7 +13,11 @@ const app = express();
 
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ['https://server1.pearl-developer.com'], // or '*' for all
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json({ limit: '100mb' }));
 app.use(express.urlencoded({ limit: '100mb', extended: true }));
 
