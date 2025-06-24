b084a7971d6ded751c9def4a6b8d8f59a1f30c83
Abhishek <abhisheks@pearlorganisation.com>
2025-06-24 14:00:50 +0530

    gsc key update

--- utils/gcloud.js
+++ utils/gcloud.js
@@ -1,16 +1,66 @@
+// const { Storage } = require('@google-cloud/storage');
+// const path = require('path');
+// const uuid = require('uuid').v4;
+
+// const storage = new Storage({
+//  keyFilename: path.resolve('gcs-key.json'),
+
+//   projectId: 'b-profiles-461910'
+// });
+
+// const bucket = storage.bucket('3bprofiles-products');
+
+// exports.uploadBufferToGCS = (buffer, filename, folder = 'uploads') => {
+//   return new Promise((resolve, reject) => {
+//     const gcsFileName = `${folder}/${uuid()}-${filename}`;
+//     const file = bucket.file(gcsFileName);
+
+//     const stream = file.createWriteStream({
+//       metadata: {
+//         contentType: 'auto'
+//       }
+//     });
+
+//     stream.on('error', err => reject(err));
+
+//     stream.on('finish', () => {
+//       // Skip makePublic() because UBLA is enabled
+//       const publicUrl = `https://storage.googleapis.com/${bucket.name}/${gcsFileName}`;
+//       resolve(publicUrl);
+//     });
+
+//     stream.end(buffer);
+//   });
+// };
+
+// gcloud.js
 const { Storage } = require('@google-cloud/storage');
-const path = require('path');
+const { SecretManagerServiceClient } = require('@google-cloud/secret-manager');
 const uuid = require('uuid').v4;
 
-const storage = new Storage({
- keyFilename: path.resolve('gcs-key.json'),
+const client = new SecretManagerServiceClient();
 
-  projectId: 'b-profiles-461910'
-});
+let storage;
 
-const bucket = storage.bucket('3bprofiles-products');
+async function getStorage() {
+  if (!storage) {
+    const [version] = await client.accessSecretVersion({
+      name: 'projects/1067354145699/secrets/gcs-service-account-key/versions/latest',
+    });
 
-exports.uploadBufferToGCS = (buffer, filename, folder = 'uploads') => {
+    const serviceAccountKey = JSON.parse(version.payload.data.toString());
+    storage = new Storage({
+      credentials: serviceAccountKey,
+      projectId: 'b-profiles-461910',
+    });
+  }
+  return storage;
+}
+
+exports.uploadBufferToGCS = async (buffer, filename, folder = 'uploads') => {
+  const storage = await getStorage();
+  const bucket = storage.bucket('3bprofiles-products');
+
   return new Promise((resolve, reject) => {
     const gcsFileName = `${folder}/${uuid()}-${filename}`;
     const file = bucket.file(gcsFileName);
@@ -21,10 +71,8 @@ exports.uploadBufferToGCS = (buffer, filename, folder = 'uploads') => {
       }
     });
 
-    stream.on('error', err => reject(err));
-
+    stream.on('error', reject);
     stream.on('finish', () => {
-      // Skip makePublic() because UBLA is enabled
       const publicUrl = `https://storage.googleapis.com/${bucket.name}/${gcsFileName}`;
       resolve(publicUrl);
     });
@@ -32,4 +80,3 @@ exports.uploadBufferToGCS = (buffer, filename, folder = 'uploads') => {
     stream.end(buffer);
   });
 };
-
