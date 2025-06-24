9622fad45298c925b929efd284dd95445e6ba7c2
Abhishek <abhisheks@pearlorganisation.com>
2025-06-24 14:28:49 +0530

    gsc key updated

--- utils/gcloud.js
+++ utils/gcloud.js
@@ -34,26 +34,24 @@
 // };
 
 // gcloud.js
-const { Storage } = require('@google-cloud/storage');
+
 const { SecretManagerServiceClient } = require('@google-cloud/secret-manager');
+const { Storage } = require('@google-cloud/storage');
 const uuid = require('uuid').v4;
 
-const client = new SecretManagerServiceClient();
-
-let storage;
+let storage; // Cached storage client
 
 async function getStorage() {
-  if (!storage) {
-    const [version] = await client.accessSecretVersion({
-      name: 'projects/1067354145699/secrets/gcs-service-account-key/versions/latest',
-    });
+  if (storage) return storage;
 
-    const serviceAccountKey = JSON.parse(version.payload.data.toString());
-    storage = new Storage({
-      credentials: serviceAccountKey,
-      projectId: 'b-profiles-461910',
-    });
-  }
+  const secretClient = new SecretManagerServiceClient();
+
+  const [version] = await secretClient.accessSecretVersion({
+    name: 'projects/1067354145699/secrets/gcs-service-account-key/versions/latest',
+  });
+
+  const key = JSON.parse(version.payload.data.toString());
+  storage = new Storage({ credentials: key, projectId: 'b-profiles-461910' });
   return storage;
 }
 
@@ -61,17 +59,18 @@ exports.uploadBufferToGCS = async (buffer, filename, folder = 'uploads') => {
   const storage = await getStorage();
   const bucket = storage.bucket('3bprofiles-products');
 
-  return new Promise((resolve, reject) => {
-    const gcsFileName = `${folder}/${uuid()}-${filename}`;
-    const file = bucket.file(gcsFileName);
+  const gcsFileName = `${folder}/${uuid()}-${filename}`;
+  const file = bucket.file(gcsFileName);
 
+  return new Promise((resolve, reject) => {
     const stream = file.createWriteStream({
       metadata: {
-        contentType: 'auto'
-      }
+        contentType: 'auto',
+      },
     });
 
-    stream.on('error', reject);
+    stream.on('error', (err) => reject(err));
+
     stream.on('finish', () => {
       const publicUrl = `https://storage.googleapis.com/${bucket.name}/${gcsFileName}`;
       resolve(publicUrl);
@@ -80,3 +79,4 @@ exports.uploadBufferToGCS = async (buffer, filename, folder = 'uploads') => {
     stream.end(buffer);
   });
 };
+
