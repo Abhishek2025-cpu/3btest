b996601274ff3f63f3482638e0df0d8a0c10fb6e
Abhishek <abhisheks@pearlorganisation.com>
2025-06-27 12:32:56 +0530

    company title changed

--- models/item.model.js
+++ models/item.model.js
@@ -21,7 +21,7 @@ const itemSchema = new mongoose.Schema({
 },
 
   shift: { type: String, enum: ['Day', 'Night'], required: true },
-  company: { type: String, enum: ['3B', 'B'], required: true },
+  company: { type: String, enum: ['B', 'BI'], required: true },
   qrCodeUrl : { type: String, required: true },
   productImageUrl: { type: String, required: true },
 }, { timestamps: true });
