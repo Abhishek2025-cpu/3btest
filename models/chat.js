9ef3ece9b3cffd030bf0b329f4c55816c2a8ea89
Abhishek <abhisheks@pearlorganisation.com>
2025-07-07 14:20:43 +0530

    senderid

--- models/chat.js
+++ models/chat.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
 
 const chatSchema = new mongoose.Schema({
-  senderId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  receiverId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+senderId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+receiverId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+
   message: { type: String, default: null },
   mediaUrl: { type: String, default: null },
   timestamp: { type: Date, default: Date.now },
