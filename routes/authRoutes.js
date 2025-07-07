07a94c3d75df030de6eea8c6cf28e4b0ec448b78
Abhishek <abhisheks@pearlorganisation.com>
2025-07-07 12:17:37 +0530

    delete user api

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -7,6 +7,7 @@ const {
   updateUser,
   getUserProfiles,
   getUserProfileById,
+  deleteUserById
 
 } = require('../controllar/authController'); 
 const otpController = require('../controllar/otpController');
@@ -20,5 +21,6 @@ router.post('/send-otp', otpController.sendOtp);
 router.post('/verify-otp', otpController.verifyOtp);
 router.get('/get-user-profiles', getUserProfiles);
 router.get('/users/:userId', getUserProfileById);
+router.delete('/users/:userId',deleteUserById)
 
 module.exports = router;
