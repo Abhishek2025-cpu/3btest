c27765b30191d7f95fdb04e7902ae986a27ca808
Abhishek <abhisheks@pearlorganisation.com>
2025-07-10 13:55:19 +0530

    cors

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ['https://server1.pearl-developer.com'], // or '*' for all
+  origin: '*', // Allow all origins
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
