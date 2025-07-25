2859886a6b17cf93bf2976eadec046924d34aef1
Abhishek <abhisheks@pearlorganisation.com>
2025-07-25 14:54:47 +0530

    controllar route fix

--- routes/gst.js
+++ routes/gst.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { verifyAndSaveGSTIN } = require('../controllers/gstController');
+const { verifyAndSaveGSTIN } = require('../controllar/gstController');
 
 router.post('/verify', verifyAndSaveGSTIN);
 
