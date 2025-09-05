bc84751e740fa7a926854b284c5dbe4176b9268e
Abhishek <abhisheks@pearlorganisation.com>
2025-09-05 17:08:10 +0530

    'hsnapi'

--- routes/hsn.js
+++ routes/hsn.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const { addHSN, getAllHSN } = require("../controllers/hsnController");
+const { addHSN, getAllHSN } = require("../controllar/hsnController");
 
-// POST: Add HSN
+
 router.post("/add", addHSN);
 
-// GET: Get all HSN
+
 router.get("/get", getAllHSN);
 
 module.exports = router;
+
