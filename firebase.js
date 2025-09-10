dc5e722f2be38a4c64e238e7d94e753ab099e035
Abhishek <abhisheks@pearlorganisation.com>
2025-09-10 14:38:30 +0530

    ss

--- firebase.js
+++ firebase.js
@@ -1,7 +1,7 @@
 const { initializeApp, applicationDefault, cert } = require("firebase-admin/app");
 const { getMessaging } = require("firebase-admin/messaging");
 const path = require("path");
-const serviceAccount = require(path.join(__dirname, "bprofiles-54714-firebase-adminsdk-fbsvc-60025e545e.json"));
+const serviceAccount = require(path.join(__dirname, "serviceAccountKey.json"));
 
 const app = initializeApp({
   credential: cert(serviceAccount)
