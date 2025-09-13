630449b1fe7ef2b13848207515f3970626effe20
Abhishek <abhisheks@pearlorganisation.com>
2025-09-13 14:37:51 +0530

    push

--- routes/notificationRoutes.js
+++ routes/notificationRoutes.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const { getUserNotifications,clearUserNotifications } = require("../controllar/notificationController");
+const { sendPushNotification } = require('../controllar/pushNotificationController');
+
 
 router.get("/notifications/:userId", getUserNotifications);
 router.delete("/notifications/clear/:userId", clearUserNotifications);
+router.post('/send-push', sendPushNotification);
 
 
 module.exports = router;
