a224a70da1c02aec2ef202938f54ee7725b7260f
Abhishek <abhisheks@pearlorganisation.com>
2025-09-15 15:03:08 +0530

    'new;

--- routes/notificationRoutes.js
+++ routes/notificationRoutes.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const { getUserNotifications,clearUserNotifications,sendPushNotification } = require("../controllar/notificationController");
-const { sendPushNotification } = require('../controllar/pushNotificationController');
+// const { sendPushNotification } = require('../controllar/pushNotificationController');
 
 
 router.get("/notifications/:userId", getUserNotifications);
 router.delete("/notifications/clear/:userId", clearUserNotifications);
-router.post('/send-push', sendPushNotification);
+// router.post('/send-push', sendPushNotification);
 router.post('/notifications/send', sendPushNotification);
 
 
