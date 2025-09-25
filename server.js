4d855bd4f46b541c929e0537cfe2ea220e9a129c
Abhishek <abhisheks@pearlorganisation.com>
2025-09-25 10:35:16 +0530

    logs

--- server.js
+++ server.js
@@ -106,7 +106,8 @@ const startServer = async () => {
   }
 
   const PORT = process.env.PORT || 8080;
-  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
+
 };
 
 startServer();
