6bd7d29e7ec9c02217e9bca55779d4f7d7f2f73e
Abhishek <abhisheks@pearlorganisation.com>
2025-10-18 12:58:36 +0530

    'okk'

--- firebase.js
+++ firebase.js
@@ -1,53 +1,4 @@
-// const { initializeApp, cert } = require("firebase-admin/app");
-// const { getMessaging } = require("firebase-admin/messaging");
-// const path = require("path");
-
-// const serviceAccount = require(path.join(__dirname, "bprofiles-54714-firebase-adminsdk-fbsvc-035dca421e.json"));
-
-// const app = initializeApp({
-//   credential: cert(serviceAccount),
-// });
-
-// const messaging = getMessaging(app);
-
-// module.exports = { app, messaging };
-
-// firebase.js
-// const { initializeApp, cert, getApps } = require("firebase-admin/app");
-// const { getMessaging } = require("firebase-admin/messaging");
-
-// let app, messaging;
-
-// function initFirebase() {
-//   if (messaging) return { app, messaging };
-
-//   let serviceAccount;
-
-//   if (process.env.FIREBASE_CONFIG) {
-//     // On Render → set FIREBASE_CONFIG env var to full JSON string
-//     serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG);
-//   } else {
-//     // Local fallback → use JSON file
-//     serviceAccount = require("./bprofiles-54714-firebase-adminsdk-fbsvc-5ae26f5109.json");
-//   }
-
-//   if (!getApps().length) {
-//     app = initializeApp({
-//       credential: cert(serviceAccount),
-//     });
-//   }
-
-//   messaging = getMessaging();
-//   return { app, messaging };
-// }
-
-// module.exports = { initFirebase };
-
-
-
-
-
-// firebase.js
+// utils/firebase.js
 const { initializeApp, cert, getApps } = require("firebase-admin/app");
 const { getMessaging } = require("firebase-admin/messaging");
 
@@ -59,11 +10,11 @@ function initFirebase() {
   let serviceAccount;
 
   if (process.env.FIREBASE_KEY) {
-    // Cloud Run injects secret as env variable
+    // Cloud Run secret
     serviceAccount = JSON.parse(process.env.FIREBASE_KEY);
   } else {
-    // fallback for local dev
-    serviceAccount = require("./serviceAccountKey.json");
+    // local fallback
+    serviceAccount = require("../serviceAccountKey.json");
   }
 
   if (!getApps().length) {
@@ -77,7 +28,3 @@ function initFirebase() {
 }
 
 module.exports = { initFirebase };
-
-
-
-
