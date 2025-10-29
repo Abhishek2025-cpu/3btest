c2970ba54646835ca1e4dc10c6595f4fb25632f4
Abhishek <abhisheks@pearlorganisation.com>
2025-10-29 11:56:45 +0530

    'fb'

--- firebase.js
+++ firebase.js
@@ -1,16 +1,11 @@
 
 const admin = require("firebase-admin");
-const fs = require("fs");
-const path = require("path");
 
 if (!admin.apps.length) {
-  const serviceAccount = process.env.FIREBASE_KEY
-    ? JSON.parse(process.env.FIREBASE_KEY)
-    : require(path.join(__dirname, "notificationkey.json")); // <-- make sure this exists
-
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+    credential: admin.credential.applicationDefault(),
   });
 }
 
 module.exports = admin;
+
